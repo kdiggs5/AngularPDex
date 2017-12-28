536f0784440e2b6157b97b8dbea35bf76dbb9c20
KDiggs <kdiggs@gmail.com>
2017-12-28 17:01:28 -0500

    Style Layout

--- AngularPDex/src/app/app.module.ts
+++ AngularPDex/src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MaterialUiModule } from './material-ui/material-ui.module';
+import { ServiceModule } from 'app/services/service.module';
+
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+
+import { ApiService } from 'app/services/api.service';
+import { PokemonService } from 'app/services/pokemon.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialUiModule,
+    ServiceModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    ApiService,
+    PokemonService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
