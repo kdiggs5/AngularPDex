e70e65651cf7c7dc6a4980ad99b3063f64fcc1bc
KDiggs <kdiggs@gmail.com>
2017-12-28 17:02:51 -0500

    Create Service module

---
+++ AngularPDex/src/app/services/service.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from 'app/services/api.service';
+import { PokemonService } from 'app/services/pokemon.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    ApiService,
+    PokemonService
+  ]
+})
+export class ServiceModule { }
